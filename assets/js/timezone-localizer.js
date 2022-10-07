0f7081fac3f940397b62a8f65fd1e35337778e83
Johannes Schirm <9149359+jojosoft@users.noreply.github.com>
2022-10-07 21:53:47 +0900

    Fix handling of invalid dates in timezone localizer

--- assets/js/timezone-localizer.js
+++ assets/js/timezone-localizer.js
@@ -2,7 +2,10 @@ function localizeTimes() {
 	times = document.querySelectorAll('[id^="time-"]');
 	for (i = 0; i < times.length; i++) {
 		d = new Date(times[i].innerHTML.replace(" ", ""));
-		times[i].innerHTML = "<small>" + d.toLocaleDateString() + "</small><br /><b>" + d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', timeZoneName: 'long' }).replace(" ", "</b><br /><small>") + "</small>";
+		// Make sure to check whether the element contained a valid date.
+		if (d instanceof Date && isFinite(d.getTime())) {
+			times[i].innerHTML = "<small>" + d.toLocaleDateString() + "</small><br /><b>" + d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', timeZoneName: 'long' }).replace(" ", "</b><br /><small>") + "</small>";
+		}
 	}
 }
 // Automatically localize times for DOM IDs that begin with "time-".
